aead5e8f70b57b09f6447f268d58b4427ab1764a
vinicius.jacques <vinicius.jacques@compasso.com.br>
2023-05-15 16:26:22 -0300

    erro fix3

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,16 +8,12 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
 })
 
 Cypress.Commands.add('login', (
-//   username = Cypress.env('USER_EMAIL'),
-//   password = Cypress.env('USER_PASSWORD'),
-    username = 'gaxinoc800@appxapi.com',
-    password = 'S%3emanudx',
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD'),
   { cacheSession = true } = {}
 ) => {
   const login = () => {
     cy.visit('/login')
-    // cy.get('#email').type(username)
-    // cy.get('#password').type(password, { log: false })
     cy.get('#email').type(username)
     cy.get('#password').type(password, { log: false })
     cy.contains('button', 'Login').click()
